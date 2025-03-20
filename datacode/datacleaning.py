167ffb8732aa6fef3f430b20979fd6afd281c097
kjiriarte <kxiriarte@gmail.com>
2025-03-20 12:34:31 -0400

    working

--- datacode/datacleaning.py
+++ datacode/datacleaning.py
@@ -2,5 +2,66 @@
 
 import pandas as pd
 
-eco = pd.read_csv("Users/karlyjae/Documents/25-spring-kiriarte/MDAcres.csv")
-print (eco)
+#load in dataset
+acres = pd.read_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/MDAcres.csv")
+#rename key to be able to merge datasets
+acres.rename(columns={'Maryland County': 'Jurisdiction'}, inplace=True)
+
+acres['Jurisdiction'] = acres['Jurisdiction'].str.replace('County', '', regex=True).str.strip()
+
+#VARIABLES TO KEEP: JURISDICTION, TOTAL Publicly Owned, GRAND TOTAL Preserved, DNR State Land Inventory
+#LPPRP Federal Park and Conservation Lands, LPPRP Local Parks and Recreation (incl. Local-Side POS)
+
+#load in dataset
+#keep all variables?? - would be good for analysis
+eco = pd.read_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/MDSocioeconomic.csv")
+#rename key to be able to merge datasets - remove 'County from strings
+eco.rename(columns={'Jurisdictions': 'Jurisdiction'}, inplace=True)
+eco['Jurisdiction'] = eco['Jurisdiction'].str.replace('County', '', regex=True).str.strip()
+eco['Jurisdiction'] = eco['Jurisdiction'].str.replace('city', 'City', regex=True).str.strip()
+
+#load in dataset
+#Keep all variables - remove anything older than 2019 to ensure most recent data is used
+
+activity = pd.read_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/MDPhysicalActivity.csv")
+activity.rename(columns={'Race/ ethnicity': 'Race'}, inplace=True)
+#remove unnecessary varaibles
+activity = activity.drop('Measure', axis=1)
+activity['Jurisdiction'] = activity['Jurisdiction'].str.replace('County', '', regex=True).str.strip()
+activity['Jurisdiction'] = activity['Jurisdiction'].str.replace("Saint Mary's", "St. Mary's", regex=True).str.strip()
+act = activity[activity['Year'] == 2019]
+
+
+act = act.pivot(index=['Jurisdiction', 'Year'] ,columns='Race', values='Value')
+act = act.reset_index()
+act = act.drop(columns=['Race'], errors='ignore')
+
+#print(act.columns.tolist())
+#print(acres)
+
+#load in dataset
+parks = pd.read_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/ParksData.csv")
+#rename key to be able to merge datasets
+parks.rename(columns={'County': 'Jurisdiction'}, inplace=True)
+#parks = parks.pivot(index=['Jurisdiction'] ,columns=parks.count())
+
+parks = parks.groupby('Jurisdiction').count().reset_index()
+parks =parks.drop(parks.columns[[2]], axis=1)
+parks.rename(columns={'Park Name': 'ParkCount'}, inplace=True)
+
+
+#print (parks)
+
+
+
+merged_df = parks.merge(acres, on='Jurisdiction', how='outer') \
+               .merge(eco, on='Jurisdiction', how='outer') \
+               .merge(act, on='Jurisdiction', how='outer')
+
+
+#print(merged_df)
+
+#code file to work with for modeling + visualization
+merged_df.to_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/mergedata.csv", index=False)
+#pd.merge()
+
---
+++ projectcode/project.py
@@ -0,0 +1,8 @@
+#to be coded
+#..
+
+import pandas as pd
+
+data = pd.read_csv("/Users/karlyjae/Documents/25-spring-kiriarte/datasets/mergedata.csv")
+
+print(data)
